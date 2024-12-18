d4c53b0b85d70d7a30c32b4d0319cef0daa49b9f
Prathamesh Katikar <pgkatikar@gmail.com>
2024-12-18 16:30:29 +0530

    first copy

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import TodoItem from function name(params) {
+    './TodoItem'
+};
+
+const TodoList = ({ todos, toggleTodo, deleteTodo }) => {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          toggleTodo={toggleTodo}
+          deleteTodo={deleteTodo}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
